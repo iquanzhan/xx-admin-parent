4c81437f9b5d534590e96e266731b54b9a0b4181
chengxiaoxiao <iquanzhan@163.com>
2022-02-21 16:23:33 +0800

    update:增加core中的异常处理+增加fastjson启动器

---
+++ xx-common/xx-common-core/src/main/java/com/chengxiaoxiao/core/constant/Constants.java
@@ -0,0 +1,20 @@
+package com.chengxiaoxiao.core.constant;
+
+/**
+ * 公共常量信息
+ *
+ * @Description:
+ * @Author: Cheng XiaoXiao
+ * @Date: 2022/2/21 0021 15:58
+ */
+public class Constants {
+    /**
+     * UTF-8 字符集
+     */
+    public static final String UTF8 = "UTF-8";
+
+    /**
+     * GBK 字符集
+     */
+    public static final String GBK = "GBK";
+}
---
+++ xx-common/xx-fastjson-spring-boot-starter/src/main/java/com/chengxiaoxiao/fastjson/FastJsonAutoConfiguration.java
@@ -0,0 +1,45 @@
+package com.chengxiaoxiao.fastjson;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.chengxiaoxiao.core.constant.Constants;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * FastJson 启动配置类
+ *
+ * @Description:
+ * @Author: Cheng XiaoXiao
+ * @Date: 2022/2/21 15:56
+ */
+public class FastJsonAutoConfiguration {
+
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverter() {
+        FastJsonHttpMessageConverter fastJsonConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        config.setSerializerFeatures(SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteNullNumberAsZero,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.WriteNullBooleanAsFalse,
+                SerializerFeature.DisableCircularReferenceDetect);
+        config.setCharset(StandardCharsets.UTF_8);
+        config.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        fastJsonConverter.setFastJsonConfig(config);
+        List<MediaType> list = new ArrayList<>();
+        list.add(MediaType.APPLICATION_JSON_UTF8);
+        fastJsonConverter.setSupportedMediaTypes(list);
+
+        return new HttpMessageConverters(fastJsonConverter);
+    }
+}
